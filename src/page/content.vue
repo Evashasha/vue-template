6e03d9eeb2454082c00fdb2fce70f46f7adf92d4
yangyankang <704799639@qq.com>
2018-04-16 13:27:58 +0800

    first commit

---
+++ src/page/content.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      this is content.vue
+    </div>
+</template>
+<script>
+export default{
+  data () {
+    return {
+      msg: 'hello vue'
+    }
+  },
+  components: {}
+}
+</script>
+<style>
+
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      this is index.vue
+    </div>
+</template>
+<script>
+export default{
+  data () {
+    return {
+      msg: 'hello vue'
+    }
+  },
+  components: {},
+  created () {
+    this.$api.get('topics', null, r => {
+      console.log(r)
+    })
+  }
+}
+</script>
+<style>
+
+</style>
